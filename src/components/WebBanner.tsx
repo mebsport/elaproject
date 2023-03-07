f6003cd82ccb3f5949291dd551c21c52aa50439f
mebsport <56307645+mebsport@users.noreply.github.com>
2023-03-07 11:18:56 -0500

    banner for top of site

---
+++ src/components/WebBanner.tsx
@@ -0,0 +1,7 @@
+import image from '/src/assets/websitebanner.png';
+
+function WebBanner() {
+  return <div style={{ backgroundImage: `url(${image})` }}>Hello World</div>;
+}
+
+export { WebBanner };
