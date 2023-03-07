a7197e266a061675d6f1f502a35bc6804b796a5a
mebsport <56307645+mebsport@users.noreply.github.com>
2023-03-07 09:47:48 -0500

    Fix right click
    ty cody

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 import Header from './components/Header';
 import YoutubeEmbed from './components/YoutubeEmbded';
 
 function App() {
-  // Disable right click
-  document.addEventListener('contextmenu', (e) => e.preventDefault());
 
+  // Disable right click
+  useEffect(() => {
+    const eventHandler = (e) => e.preventDefault();
+  
+    window.addEventListener("contextmenu", eventHandler)
+    return () => window.removeEventListener("contextmenu", eventHandler)
+  }, [])
   return (
     <div className='App'>
       <Header />
       <div></div>
       <h1>ELA PROJECT</h1>
       <p>Police violence and brutality is bad...</p>
-      
+
       <YoutubeEmbed embedId='E0RKl7Q8HSc' />
     </div>
   );
