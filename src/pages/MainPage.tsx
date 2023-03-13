5a9b56096d82a12f820862391d3fcff8a2b0103d
mebsport <56307645+mebsport@users.noreply.github.com>
2023-03-13 10:22:06 -0400

    Created Placeholder For Thesis Statement

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage() {
+  return (
+    <div className='Thesis'>
+      <h1>[Insert Thesis Statemnt Here]</h1>
+    </div>
+  );
+}
+
+export default MainPage;
