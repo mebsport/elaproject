c7a148dd8d209d387b80352f4861c7bd149bc0c0
mebsport <56307645+mebsport@users.noreply.github.com>
2023-03-15 08:18:59 -0400

    Fix Scaling For Portrait Devices

--- src/pages/ResearchQuestion.tsx
+++ src/pages/ResearchQuestion.tsx
@@ -4,13 +4,19 @@ import styled from 'styled-components';
 const Container = styled.div`
   display: flex;
 
-  * {
+  @media screen and (orientation: landscape) {
     flex: 1;
     padding-top: 5%;
   }
 
   &:nth-child(odd) {
     flex-direction: row-reverse;
+  }
+
+  @media screen and (orientation: portrait) {
+    flex: 1;
+    padding-top: 5%;
+    flex-direction: column-reverse !important;
   }
 
   .Text {
@@ -31,6 +37,11 @@ const Photo = styled.img`
   max-width: 50%;
   padding-left: 10px;
   padding-right: 10px;
+
+  @media screen and (orientation: portrait) {
+    max-width: 85% !important;
+    align-self: center;
+  }
 `;
 
 const ResearchQuestion = ({
